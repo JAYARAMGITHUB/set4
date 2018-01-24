c9764c623c149e156b506a30767c88411c81144e
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-01-24 16:51:44 +0530

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,t;
+printf("enter the values of a and b before swapping\n");
+scanf("%d %d",&a,&b);
+t=a;
+a=b;
+b=t;
+printf("the values of a and b after swapping: %d,%d",a,b);
+}
