d30db4dcbddf8e6adfe505a2f5875eb8360a2c15
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-01-31 15:48:04 +0530

    Create 35.C

---
+++ 35.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[50];
+int len,i,number=0;
+printf("enter the string:");
+scanf("%[\n]d",&a);
+len=strlen(a);
+for(i=0;i<len;i++)
+{
+if(a>=0&&a<=9)
+number=number+1;
+}
+printf("there are %d numerics",number);
+
+}
