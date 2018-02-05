19af4074bd394d592b7c9e5848ff22c4da184c45
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-02-05 15:56:53 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,count =0,f;
+printf("\n  enter number \n");
+scanf("%d",&a);
+while(a>0)
+{
+a=a/10;
+f=a;
+count++;}
+printf("\n number of digits%d",count);
+
+}
