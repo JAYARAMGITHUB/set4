f8879e786768e466be191458122af9d3cdb2554d
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-02-09 15:42:02 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("\n enter a number");
+scanf("%d",num);
+if(num%2==0)
+{
+printf("even:%d",num);
+}
+else
+{
+printf("less_even:%d",num-1);
+}
+return(0);
+}
